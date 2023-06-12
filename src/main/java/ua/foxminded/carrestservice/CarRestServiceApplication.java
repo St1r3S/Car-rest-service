ac5ca021a12e31ae33405b3ec08aa01ec8bf7cb0
St1r3S <mikmuael.steam@gmail.com>
2023-06-12 17:54:44 +0300

    Initial commit

---
+++ src/main/java/ua/foxminded/carrestservice/CarRestServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.foxminded.carrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/foxminded/carrestservice/CarRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.foxminded.carrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
