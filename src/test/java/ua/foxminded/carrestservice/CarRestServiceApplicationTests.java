5cbec2b921f9a51a6182ffca4edc3129fcbd2ac7
St1r3S <mikmuael.steam@gmail.com>
2023-08-03 18:52:56 +0300

    Add context loads test

--- src/test/java/ua/foxminded/carrestservice/CarRestServiceApplicationTests.java
+++ src/test/java/ua/foxminded/carrestservice/CarRestServiceApplicationTests.java
@@ -1,13 +1,21 @@
 package ua.foxminded.carrestservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ua.foxminded.carrestservice.service.SecurityService;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CarRestServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    SecurityService service;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(service);
+    }
 
 }
